d108503c6c248bb47ec581646f80170fce1d1e10
Glenn <glenn.troncquo@student.howest.be>
2021-12-08 11:46:00 +0100

    ar added

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public void HandleButton()
+    {
+        Debug.Log("Next pizza");
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
